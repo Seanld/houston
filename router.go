b0c848cd652bca3e2c498833a7517b3c8b0f2d92
Sean Wilkerson <me@seanld.xyz>
2022-01-25 12:48:01 -0700

    Nearly done with basic routing functionality

---
+++ router.go
@@ -0,0 +1,52 @@
+package houston
+
+
+import (
+	"net"
+	"path"
+)
+
+
+type RouteHandler func(net.Conn)
+
+
+type Route struct {
+	Path     string
+	Handler  RouteHandler
+}
+
+
+type RouterOptions struct {
+	ErrorHandler  RouteHandler
+}
+
+
+type Router struct {
+	Routes        []Route
+	ErrorHandler  RouteHandler
+}
+
+
+func NewRouter(options RouterOptions) Router {
+	newRouter := Router{ErrorHandler: options.ErrorHandler}
+	return newRouter
+}
+
+
+// Get the handler for a given route. If no route matches a
+// handler, then return the default error handler.
+func (r Router) GetRouteHandler(targetPath string) RouteHandler {
+	cleanedPath := path.Clean(targetPath)
+	for _, elem := range r.Routes {
+		if elem.Path == cleanedPath {
+			return elem.Handler
+		}
+	}
+	return r.ErrorHandler
+}
+
+
+func (r Router) AddRoute(targetPath string, handler RouteHandler) {
+	newRoute := Route{path.Clean(targetPath), handler}
+	r.Routes = append(r.Routes, newRoute)
+}
